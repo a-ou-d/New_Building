e9211256e49dbbf2406c37fe4ac5bd87bbdac967
a-ou-d <148048020+a-ou-d@users.noreply.github.com>
2023-12-28 17:35:35 +0900

    04. [FIX] JUMP
    중력 컨트롤 실패 !

--- Assets/Flooded_Grounds/Scripts/FPSController/CharController_Motor.cs
+++ Assets/Flooded_Grounds/Scripts/FPSController/CharController_Motor.cs
@@ -5,78 +5,105 @@ using UnityEngine;
 public class CharController_Motor : MonoBehaviour
 {
 
-	public float speed = 10.0f;
-	public float sensitivity = 30.0f;
-	public float WaterHeight = 15.5f;
-	CharacterController character;
-	public GameObject cam;
-	float moveFB, moveLR;
-	float rotX, rotY;
-	public bool webGLRightClickRotation = true;
-	float gravity = -9.8f;
+    CharacterController character;
+    public GameObject cam;
+
+    public float speed = 10.0f;
+    public float sensitivity = 30.0f;
+    public float WaterHeight = 15.5f;
+    float gravity = -9.8f;
+
+    float moveFB, moveLR;
+    float rotX, rotY;
+    public bool webGLRightClickRotation = true;
 
 
-	void Start()
-	{
-		//LockCursor ();
-		character = GetComponent<CharacterController>();
+    void Start()
+    {
+        //LockCursor ();
+        character = GetComponent<CharacterController>();
 
-		if (Application.isEditor)
-		{
-			webGLRightClickRotation = false;
-			sensitivity = sensitivity * 1.5f;
-		}
-	}
-
-
-	void CheckForWaterHeight()
-	{
-		if (transform.position.y < WaterHeight)
-		{
-			gravity = 0f;
-		}
-		else
-		{
-			gravity = -9.8f;
-		}
-	}
+        if (Application.isEditor)
+        {
+            webGLRightClickRotation = false;
+            sensitivity = sensitivity * 1.5f;
+        }
+    }
 
 
-	void Update()
-	{
-		moveFB = Input.GetAxis("Horizontal") * speed;
-		moveLR = Input.GetAxis("Vertical") * speed;
-
-		rotX = Input.GetAxis("Mouse X") * sensitivity;
-		rotY = Input.GetAxis("Mouse Y") * sensitivity;
-
-		//rotX = Input.GetKey (KeyCode.Joystick1Button4);
-		//rotY = Input.GetKey (KeyCode.Joystick1Button5);
-
-		CheckForWaterHeight();
-
-		Vector3 movement = new Vector3(moveFB, gravity, moveLR);
-
-		if (webGLRightClickRotation)
-		{
-			if (Input.GetKey(KeyCode.Mouse0))
-			{
-				CameraRotation(cam, rotX, rotY);
-			}
-		}
-		else if (!webGLRightClickRotation)
-		{
-			CameraRotation(cam, rotX, rotY);
-		}
-
-		movement = transform.rotation * movement;
-		character.Move(movement * Time.deltaTime);
-	}
+    void CheckForWaterHeight()
+    {
+        if (transform.position.y < WaterHeight)
+        {
+            gravity = 0f;
+        }
+        else
+        {
+            gravity = -9.8f;
+        }
+    }
 
 
-	void CameraRotation(GameObject cam, float rotX, float rotY)
-	{
-		transform.Rotate(0, rotX * Time.deltaTime, 0);
-		cam.transform.Rotate(-rotY * Time.deltaTime, 0, 0);
-	}
+    void Update()
+    {
+        moveFB = Input.GetAxis("Horizontal") * speed;
+        moveLR = Input.GetAxis("Vertical") * speed;
+
+        rotX = Input.GetAxis("Mouse X") * sensitivity;
+        rotY = Input.GetAxis("Mouse Y") * sensitivity;
+
+        //rotX = Input.GetKey (KeyCode.Joystick1Button4);
+        //rotY = Input.GetKey (KeyCode.Joystick1Button5);
+
+        CheckForWaterHeight();
+
+        Vector3 movement = new Vector3(moveFB, gravity, moveLR);
+
+        if (webGLRightClickRotation)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                CameraRotation(cam, rotX, rotY);
+            }
+        }
+        else if (!webGLRightClickRotation)
+        {
+            CameraRotation(cam, rotX, rotY);
+        }
+
+        movement = transform.rotation * movement;
+        character.Move(movement * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.E)) { Interact(); }
+
+        if (Input.GetKeyDown(KeyCode.Tab)) { OpenCraftingBook(); }
+
+        if (Input.GetKeyDown(KeyCode.I)) { OpenInventory(); }
+    }
+
+
+    void CameraRotation(GameObject cam, float rotX, float rotY)
+    {
+        transform.Rotate(0, rotX * Time.deltaTime, 0);
+        cam.transform.Rotate(-rotY * Time.deltaTime, 0, 0);
+    }
+
+
+    void Interact()
+    {
+        // 상호작용
+    }
+
+
+    void OpenCraftingBook()
+    {
+        // 건축 제작법..
+    }
+
+
+    void OpenInventory()
+    {
+        // 인벤토리
+    }
+
 }
--- Assets/Scripts/Controller/CharacterController.cs
+++ Assets/Scripts/Controller/CharacterController.cs
@@ -2,22 +2,25 @@
 //using System.Collections.Generic;
 //using UnityEngine;
 
-//public class PlayerController : MonoBehaviour
+//public class CharController_Motor : MonoBehaviour
 //{
 
 //    CharacterController character;
 //    public GameObject cam;
-
-//    public bool webGLRightClickRotation = true;
 
 //    public float speed = 10.0f;
 //    public float sensitivity = 30.0f;
 //    public float WaterHeight = 15.5f;
 //    float gravity = -9.8f;
 
+//    float moveFB, moveLR;
+//    float rotX, rotY;
+//    public bool webGLRightClickRotation = true;
+
 
 //    void Start()
 //    {
+//        //LockCursor ();
 //        character = GetComponent<CharacterController>();
 
 //        if (Application.isEditor)
@@ -43,36 +46,39 @@
 
 //    void Update()
 //    {
-//        float moveFB = Input.GetKey(KeyCode.W) ? 1f : Input.GetKey(KeyCode.S) ? -1f : 0f;
-//        float moveLR = Input.GetKey(KeyCode.A) ? -1f : Input.GetKey(KeyCode.D) ? 1f : 0f;
+//        moveFB = Input.GetAxis("Horizontal") * speed;
+//        moveLR = Input.GetAxis("Vertical") * speed;
 
-//        Vector3 movement = new Vector3(moveLR, 0, moveFB) * speed;
+//        rotX = Input.GetAxis("Mouse X") * sensitivity;
+//        rotY = Input.GetAxis("Mouse Y") * sensitivity;
+
+//        //rotX = Input.GetKey (KeyCode.Joystick1Button4);
+//        //rotY = Input.GetKey (KeyCode.Joystick1Button5);
+
 //        CheckForWaterHeight();
+
+//        Vector3 movement = new Vector3(moveFB, gravity, moveLR);
+
+//        if (webGLRightClickRotation)
+//        {
+//            if (Input.GetKey(KeyCode.Mouse0))
+//            {
+//                CameraRotation(cam, rotX, rotY);
+//            }
+//        }
+//        else if (!webGLRightClickRotation)
+//        {
+//            CameraRotation(cam, rotX, rotY);
+//        }
 
 //        movement = transform.rotation * movement;
 //        character.Move(movement * Time.deltaTime);
 
-//        if ((webGLRightClickRotation && Input.GetKey(KeyCode.Mouse0)) || !webGLRightClickRotation)
-//        {
-//            float rotX = Input.GetAxis("Mouse X") * sensitivity;
-//            float rotY = Input.GetAxis("Mouse Y") * sensitivity;
-//            CameraRotation(cam, rotX, rotY);
-//        }
+//        if (Input.GetKeyDown(KeyCode.E)) { Interact(); }
 
-//        if (Input.GetKeyDown(KeyCode.E))
-//        {
-//            //Interact();
-//        }
+//        if (Input.GetKeyDown(KeyCode.Tab)) { OpenCraftingBook(); }
 
-//        if (Input.GetKeyDown(KeyCode.Tab))
-//        {
-//            //OpenCraftingBook();
-//        }
-
-//        if (Input.GetKeyDown(KeyCode.I))
-//        {
-//            //OpenInventory();
-//        }
+//        if (Input.GetKeyDown(KeyCode.I)) { OpenInventory(); }
 //    }
 
 
@@ -88,13 +94,144 @@
 //        // 상호작용
 //    }
 
+
 //    void OpenCraftingBook()
 //    {
 //        // 건축 제작법..
 //    }
 
+
 //    void OpenInventory()
 //    {
 //        // 인벤토리
 //    }
+
+//}
+
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class CharController_Motor : MonoBehaviour
+//{
+
+//    CharacterController character;
+//    public GameObject cam;
+
+//    public float speed = 10.0f;
+//    public float sensitivity = 30.0f;
+//    public float WaterHeight = 15.5f;
+//    float originalGravity = -9.8f;
+//    float gravity;
+
+//    float moveFB, moveLR;
+//    float rotX, rotY;
+//    public bool webGLRightClickRotation = true;
+
+//    public float jumpStrength = 5.0f;
+//    public float jumpDuration = 1.0f;
+//    private bool isJumping;
+//    private float jumpTimer;
+
+
+//    void Start()
+//    {
+//        //LockCursor ();
+//        character = GetComponent<CharacterController>();
+//        gravity = originalGravity;
+
+//        if (Application.isEditor)
+//        {
+//            webGLRightClickRotation = false;
+//            sensitivity = sensitivity * 1.5f;
+//        }
+//    }
+
+
+//    void CheckForWaterHeight()
+//    {
+//        if (transform.position.y < WaterHeight)
+//        {
+//            gravity = 0f;
+//        }
+//        else
+//        {
+//            gravity = originalGravity;
+//        }
+//    }
+
+
+//    void Update()
+//    {
+//        moveFB = Input.GetAxis("Horizontal") * speed;
+//        moveLR = Input.GetAxis("Vertical") * speed;
+
+//        rotX = Input.GetAxis("Mouse X") * sensitivity;
+//        rotY = Input.GetAxis("Mouse Y") * sensitivity;
+
+//        CheckForWaterHeight();
+
+//        Vector3 movement = new Vector3(moveFB, 0, moveLR);
+//        movement = transform.rotation * movement;
+
+//        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+//        {
+//            isJumping = true;
+//            jumpTimer = jumpDuration;
+//        }
+
+//        if (isJumping)
+//        {
+//            if (jumpTimer > 0)
+//            {
+//                movement.y = jumpStrength;
+//                jumpTimer -= Time.deltaTime;
+//            }
+//            else
+//            {
+//                isJumping = false;
+//            }
+//        }
+
+//        if (!isJumping && gravity != 0f)
+//        {
+//            movement.y += gravity * Time.deltaTime;
+//        }
+
+//        character.Move(movement * Time.deltaTime);
+
+//        CameraRotation(cam, rotX, rotY);
+
+//        if (Input.GetKeyDown(KeyCode.E)) { Interact(); }
+//        if (Input.GetKeyDown(KeyCode.Tab)) { OpenCraftingBook(); }
+//        if (Input.GetKeyDown(KeyCode.I)) { OpenInventory(); }
+//    }
+
+
+//    void CameraRotation(GameObject cam, float rotX, float rotY)
+//    {
+//        transform.Rotate(0, rotX * Time.deltaTime, 0);
+//        cam.transform.Rotate(-rotY * Time.deltaTime, 0, 0);
+//    }
+
+
+//    void Interact()
+//    {
+//        // 상호작용
+//    }
+
+
+//    void OpenCraftingBook()
+//    {
+//        // 건축 제작법..
+//    }
+
+
+//    void OpenInventory()
+//    {
+//        // 인벤토리
+//    }
+
 //}
